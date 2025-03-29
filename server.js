935e9f08c379e48c4a3284f415c45282689a683f
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-03-29 23:51:25 +0000

    cambios

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server runningwtertrwert on port ${PORT}`);
+  console.log(`Servidor Backend corriendo en el puerto ${PORT}`);
 });
