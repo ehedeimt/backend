0ea1261cc79e73296f53881913e8660192ecdadb
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-03-30 00:30:59 +0000

    bbdd

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Conectar con PostgreSQL (cambia los valores según tu configuración)
-const sequelize = new Sequelize('postgres://usuario:contraseña@localhost:5432/mi_basedatos');
+const sequelize = new Sequelize('postgresql://postgres:QuzItzlTJzMmGKHuMjAtoIdbJUNZoKBF@crossover.proxy.rlwy.net:16391/railway');
 
 const Name = sequelize.define('Name', {
     name: { type: DataTypes.STRING, allowNull: false }
