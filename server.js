33e9d57719ba6653b18abe0a6f430edfbe7dad01
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-03-30 00:45:32 +0000

    servidor respondiendo

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { Sequelize, DataTypes } = require('sequelize');
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
-// Conectar con PostgreSQL
-const sequelize = new Sequelize('postgresql://postgres:QuzItzlTJzMmGKHuMjAtoIdbJUNZoKBF@crossover.proxy.rlwy.net:16391/railway');
-
-const Name = sequelize.define('Name', {
-    name: { type: DataTypes.STRING, allowNull: false }
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-sequelize.sync();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
