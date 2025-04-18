0b8505a71b3b161a0fb3255b63074f8d9b3c8238
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-18 23:22:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth');
+const usuarioRoutes = require('./routes/usuarios');
 require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', authRoutes);
+app.use('/api', usuarioRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
